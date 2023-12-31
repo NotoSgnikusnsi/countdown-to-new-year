8ed3ea2d8583ffdefeeb7a7cd4b30cadc8a099e7
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-12-31 18:42:50 +0900

    feat: 年明けまで何秒かカウントダウンする機能を実装する

--- public/index.js
+++ public/index.js
@@ -1,4 +1,16 @@
-window.onload = async () => {
-  const res = await fetch('/hello');
-  document.getElementById('message').innerText = await res.text();
-};
+function getRemainingTime() {
+  const currentYear = new Date().getFullYear();
+  const currentTime = new Date();
+  const targetTime = new Date(currentYear + 1, 0, 1, 0, 0, 0);
+
+  const remainingTime = ((targetTime - currentTime) / 1000).toFixed(3);
+
+  return remainingTime;
+}
+
+window.onload = () => {
+  const showTimeArea = document.getElementById("time");
+  setInterval(() => {
+    showTimeArea.innerText = getRemainingTime();
+  }, 1);
+}

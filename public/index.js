ac9de47a7df632eea2f9b98cedc91516bdd05c7f
notosgnikusnsi <notosgnikusnsi@gmail.com>
2023-12-31 21:46:33 +0900

    feat: 新年1時間は表示される文字が変化し、クラッカーがなるように実装する

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+import * as party from "https://cdn.jsdelivr.net/npm/canvas-confetti@1.3.2/dist/confetti.browser.min.js";
 function getRemainingTime() {
   const currentYear = new Date().getFullYear();
   const currentTime = new Date();
@@ -9,8 +10,23 @@ function getRemainingTime() {
 }
 
 window.onload = () => {
-  const showTimeArea = document.getElementById("time");
+  const showTimeArea = document.getElementById('time');
   setInterval(() => {
-    showTimeArea.innerText = getRemainingTime();
+    const remainingTime = getRemainingTime();
+    if (remainingTime > 31532400) {
+      showTimeArea.innerText = 'Happy New Year!';
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: 0.8, y: 0.8 }
+      });
+      confetti({
+        particleCount: 100,
+        spread: 70,
+        origin: { x: 0.2, y: 0.8 }
+      });
+    } else {
+      showTimeArea.innerText = remainingTime;
+    }
   }, 1);
-}
+};
